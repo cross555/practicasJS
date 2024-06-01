7b1ef940d0f51075966d1a4f43721b9226c485fa
razvasile <razvanalbu32@gmail.com>
2024-06-01 17:59:24 +0200

    segundo commit

---
+++ models/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require(`path`);
+class Server {
+    constructor () {
+        this.port = process.env.PORT;
+        this.app = express();
+        this.middleware();
+        this.routes();
+
+    }
+    middleware() {
+           this.app.use(express.static(`public`));
+    }
+    routes() {
+           this.app.use(`/index` , (req,res)=>{
+            res.sendFile(path.resolve(`public/index.html`));
+           });
+           this.app.use(`/contact` , (req,res)=>{
+            res.sendFile(path.resolve(`public/contact.html`));
+           });
+           this.app.use(`/localizacion` , (req,res)=>{
+            res.sendFile(path.resolve(`public/localizacion.html`));
+           });
+           this.app.use(`/*` , (req,res)=>{
+            res.sendFile(path.resolve(`public/404error.html`));
+           });
+    }
+    listen(){
+        this.app.listen(this.port,()=>{console.log(`http://localhost:8080`)});
+    }
+}
+
+module.exports = Server
